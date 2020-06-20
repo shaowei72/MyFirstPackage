3780318763fb5604ea89208500122e72cde4dd45
Ying Shao Wei <shaowei@singtel.com>
2020-06-20 11:27:12 +0800

    For Week 4 assignment

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -96,8 +96,7 @@ fars_read_years <- function(years) {
 #' @export
 fars_summarize_years <- function(years) {
   dat_list <- fars_read_years(years)
-  dplyr::bind_rows(dat_list)
-  %>%
+  dplyr::bind_rows(dat_list) %>%
     dplyr::group_by(year, MONTH) %>%
     dplyr::summarize(n = n()) %>%
     tidyr::spread(year, n)
