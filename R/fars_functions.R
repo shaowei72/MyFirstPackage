2c7703506c8f9699b15473d543690955c906184d
Ying Shao Wei <shaowei@singtel.com>
2020-06-20 11:40:46 +0800

    Trying

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -98,7 +98,7 @@ fars_summarize_years <- function(years) {
   dat_list <- fars_read_years(years)
   dplyr::bind_rows(dat_list) %>%
     dplyr::group_by(year, MONTH) %>%
-    dplyr::summarize(n = n()) %>%
+    dplyr::summarise(n = n()) %>%
     tidyr::spread(year, n)
 }
 
